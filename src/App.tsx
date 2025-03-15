e02546a91214bdd872e8d508ce37be1383bd785c
Manikandan M <mmanikandan281@gmail.com>
2025-03-15 19:39:34 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./App.css";
+import Container from "@material-ui/core/Container";
+import { Dict } from "./Components/Dict";
+import {
+  createTheme,
+  ThemeProvider,
+  ThemeProviderProps,
+} from "@material-ui/core/styles";
+
+const darkTheme = createTheme({
+  palette: {
+    type: "dark",
+  },
+});
+
+function App() {
+  return (
+    <div>
+      <ThemeProvider theme={darkTheme}>
+        <Container maxWidth="md" style={{ textAlign: "center" }}>
+          <Dict />
+        </Container>
+      </ThemeProvider>
+    </div>
+  );
+}
+
+export default App;
+{/* export NODE_OPTIONS=--openssl-legacy-provider //if any try catch error occurs use this to run the code */}
